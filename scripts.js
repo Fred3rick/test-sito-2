b7ae29b3f4e6f522ea09cc8f5ebf9c646b01220b
Fred3rick <62725164+Fred3rick@users.noreply.github.com>
2026-05-06 12:16:50 +0200

    Upload dei primi files

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const status = document.getElementById('status');
+    status.innerText = 'Online e Funzionante!';
+    status.style.color = '#00ff00';
+});
+
+function saluta() {
+    alert('Ciao Fred3Rick25! Il tuo sito su GitHub sta funzionando alla grande.');
+}
